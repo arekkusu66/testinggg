09d64174d65cfe30a26ad21876661ae5c19f90ff
arekkusu66 <bokuwaarekkusu66@gmail.com>
2026-03-10 16:18:23 +0200

    second commit

--- main.go
+++ main.go
@@ -2,26 +2,9 @@ package main
 
 import (
     "fmt"
-    _ "github.com/mattn/go-sqlite3"
+    "encoding/hex"
 )
 
-var q = `CREATE TABLE users (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    username TEXT NOT NULL UNIQUE CHECK(username <> ''),
-    join_date DATETIME NOT NULL DEFAUL DATETIME('now')
-)`
-
 func main() {
-    db, err := sql.Open("sqlite3", "mydb.db")
-
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    defer db.Close()
-
-    if _, err := db.Exec(q); err != nil {
-        fmt.Println(err)
-        return
-    }
+    fmt.Println(hex.EncodeToString([]byte("hello!")))
 }
