ee7e7a10bc6751166d7ac2d1656010fb983fa71e
arekkusu66 <bokuwaarekkusu66@gmail.com>
2026-03-11 13:01:34 +0200

    hi lol

--- main.go
+++ main.go
@@ -2,16 +2,51 @@ package main
 
 import (
     "fmt"
+    "os"
+    "bufio"
+    "strings"
     "time"
 )
 
+
+type Resp struct {
+    Text string
+    Ok   bool
+}
+
+
+type Client struct{
+    Name string
+    Resp chan Resp
+}
+
+
 func main() {
-    ch := make(chan string, 1)
+    var queuedUsers = make(chan Client, 1)
 
     go func() {
-        time.Sleep(time.Second * 6)
-        ch <- "jjj"
+        for u := range queuedUsers {
+            time.Sleep(time.Second * 6)
+            u.Resp <- Resp{
+                Text: strings.ToUpper(u.Name),
+                Ok: true,
+            }
+        }
     }()
 
-    fmt.Println(<-ch)
+    var scanner = bufio.NewScanner(os.Stdin)
+
+    for scanner.Scan() {
+        var client = Client{
+            Name: scanner.Text(),
+            Resp: make(chan Resp, 1),
+        }
+
+        queuedUsers <- client
+
+        go func(c Client) {
+            var resp = <-c.Resp
+            fmt.Println(resp.Ok, resp.Text)
+        }(client)
+    }
 }
